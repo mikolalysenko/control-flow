f710449607b8f557e079bdddf460fe424ff3a8c2
Mikola Lysenko <mikolalysenko@gmail.com>
2014-01-04 16:20:42 -0600

    working on test cases

--- test/basic.js
+++ test/basic.js
@@ -11,10 +11,10 @@ function testCode(t, code, remark) {
   var ast = esprima.parse(code)
   var cfg = controlFlow(ast)
 
-  console.log(util.inspect(cfg, {depth:10}))
+  //console.log(util.inspect(cfg, {depth:10}))
 
   var regen = toJS(cfg)
-  console.log(regen)
+  //console.log(regen)
 
   var reference = vm.runInNewContext(code)
   var result = vm.runInNewContext(regen)
@@ -23,7 +23,25 @@ function testCode(t, code, remark) {
 }
 
 tape("basic test", function(t) {
+  testCode(t, "")
+  testCode(t, "null")
+  testCode(t, "false")
+  testCode(t, "true")
   testCode(t, "1")
+  testCode(t, "'foo'")
+  t.end()
+})
+
+tape("expressions", function(t) {
   testCode(t, "1+2")
+  testCode(t, "(1+2)*3")
+  testCode(t, "var a")
+  testCode(t, "var a=1,b=2,c;c=a;a=b;b=c;10*a+b")
+  testCode(t, "1 || 2")
+  testCode(t, "false || 3")
+  testCode(t, "2 && 3")
+  testCode(t, "null && 'a'")
+  testCode(t, "true ? 'a' : 'b'")
+  testCode(t, "0 ? 'a' : 'b'")
   t.end()
 })
